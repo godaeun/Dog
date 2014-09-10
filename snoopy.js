e67ac8605069bf69787c44c96f1ff13733c1ddc0
heesuchoi1 <heesu7563@gmail.com>
2014-09-10 21:07:36 +0900

    sdfsdf

--- snoopy.js
+++ snoopy.js
@@ -2,8 +2,8 @@ function setup() {
   createCanvas(680, 600);
   background(0);
   stroke(200);
-  noFill();
+
     rect(0, 0, 680, 20); // x, y, width, height
-    rect(1, 34, 20, 20);
-    rect(1, 68, 20, 20);
+    rect(, , 20, 20);
+    rect(, , 20, 20);
 }
