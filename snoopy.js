e13f84d6ac3ac20731914495718a5701cbefdd01
heesuchoi1 <heesu7563@gmail.com>
2014-09-10 21:02:43 +0900

    sdfaerthgdfgdg

--- snoopy.js
+++ snoopy.js
@@ -4,6 +4,6 @@ function setup() {
   stroke(200);
   noFill();
     rect(0, 0, 680, 20); // x, y, width, height
-    rect(0, 34, 20, 20);
-    rect(0, 68, 20, 20);
+    rect(1, 34, 20, 20);
+    rect(1, 68, 20, 20);
 }
