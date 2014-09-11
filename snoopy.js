2cc8887f6e7540907ab4a9960f0f2f4e9a8f1990
godaeun <gowooda@naver.com>
2014-09-11 15:00:06 +0900

    ddfdsf

--- snoopy.js
+++ snoopy.js
@@ -1,16 +1,9 @@
 function setup() {
   createCanvas(680, 600);
 
-    // 0 : black, 255 : white
-    //background(red, gree, blue);
-    background(57, 159, 231);
-
-    // stroke(red, green, blue);
-    stroke(255, 0, 0);
-    // ... 빨간색으로 그리기 (rect, line, ellipse등)
-    stroke(0, 255, 0);
-    // ... 녹색으로 그리기 (rect, line, ellipse등)
-    fill(255, 232, 204);    
+   ackground(0);
+   stroke(255);
+   noFill();
 
     rect(0,0,20,20);
     rect(20,0,20,20);
